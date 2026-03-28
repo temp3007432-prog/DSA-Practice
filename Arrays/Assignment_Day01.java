f8213520fffee13249d4a2fc5179e4dd1d5376d7
Prathamesh Pande <pandepm28@gmail.com>
2026-03-28 19:01:36 +0530

    Updating file structure

--- Arrays/Assignment_Day01.java
+++ Arrays/Assignment_Day01.java
@@ -1,3 +1,4 @@
+package Arrays;
 public class Assignment_Day01 {
 
 	public static void main(String[] args) {
--- Arrays/Diagonal.java
+++ Arrays/Diagonal.java
@@ -1,3 +1,4 @@
+package Arrays;
 
 public class Diagonal {
 	public static void main(String[] args) {
